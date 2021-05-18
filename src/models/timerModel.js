396eafb02b35af17fc193fcfa8e236a091641021
Fenniche-Ouahab <fennicheouahab1993@gmail.com>
2021-05-18 19:59:20 +0200

    adding initial CRUD of timer

---
+++ src/models/timerModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+/**
+ * Define timer model
+ */
+const TimerSchema = mongoose.Schema({
+    // id_timer : {
+    //     type: String
+    // },
+    description: {
+        type: String
+    },
+
+    taskType: {
+        type: String
+    },
+
+    // user: {
+
+    // },
+
+    startTime: {
+        type: Date,
+        default: Date.now
+    },
+
+    duration: {
+        type: Date,
+        default: Date.now
+    },
+
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now
+    }
+}
+);
+
+module.exports = mongoose.model('Timer', TimerSchema);
