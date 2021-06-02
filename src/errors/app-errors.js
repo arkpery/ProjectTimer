6960891463c899032c5437edfdcd44f474dccd82
Fenniche-Ouahab <fennicheouahab1993@gmail.com>
2021-06-02 22:32:50 +0200

    add timers route and model, update update file path utils

--- src/errors/app-errors.js
+++ src/errors/app-errors.js
@@ -1,10 +1,8 @@
 let AppError = class AppError extends Error {
     constructor(message, status) {
         super();
-
         Error.captureStackTrace(this, this.constructor);
         this.name = this.constructor.name;
-
         this.message = message || 'Error Server';
         this.status = status || 500;
     }
