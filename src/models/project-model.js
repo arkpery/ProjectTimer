d0f457244b9d2fbc5108b89cd25dbb7f619b1123
arkpery <thomas.simoes.dev@gmail.com>
2021-06-06 20:52:35 +0200

    i18n model

--- src/models/project-model.js
+++ src/models/project-model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const translator = require("../services/translate");
 
 /**
  * Define project model
@@ -6,16 +7,16 @@ const mongoose = require('mongoose');
 const projectSchema = mongoose.Schema({
     name: {
         type: String,
-        required: "Le nom du projet est requis",
+        required: translator.translate("PROJECT_NAME_REQUIRED"),
         match: /[a-z]/,
-        minlength: [3, 'Project name too short !'],
-        maxlength: [20, 'Project too long !'],
+        minlength: [3, translator.translate("PROJECT_NAME_TOO_SHORT")],
+        maxlength: [20, translator.translate("PROJECT_TOO_LONG")],
     },
 
     admin: {
         type: mongoose.Types.ObjectId,
         ref: 'User',
-        require: "Admin is required"
+        require: translator.translate("ADMIN_REQUIRED")
     },
 
     groups: [{
